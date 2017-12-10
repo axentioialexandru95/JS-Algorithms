a2f23ec0ca15449c1799b00632314e91833fe5f3
Alexandru <axentioialexandru95@gmail.com>
2017-12-10 12:48:52 +0200

    Added prime-factorization

--- verify-prime-number.js
+++ verify-prime-number.js
@@ -1,15 +1,33 @@
 function isPrime(n) {
+  // Divisor
   var divisor = 2;
 
+  // As long as the number is bigger than 2
   while(n > divisor){
+    // And if the number divisible by 2 == 0
     if(n % divisor == 0){
+      // Then return false
       return false;
     }
+    // Else
     else {
+      // Increments the divisor
       divisor++;
     }
+    // Otherwise, return true
     return true;
   }
 }
 
+// This will give the answer of undefined
 console.log(isPrime(2));
+
+// False
+console.log(isPrime(4));
+
+// True
+console.log(isPrime(5));
+
+// False
+console.log(isPrime(12));
+console.log(isPrime(24));
